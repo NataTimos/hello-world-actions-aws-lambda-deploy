e85d550143acc1b1fe788ac95a6cd19e8aba7d7d
Adam Grbac <adam.grbac@gmail.com>
2020-09-21 18:43:18 +0100

    Changed API Output

--- lambda_function.py
+++ lambda_function.py
@@ -6,5 +6,5 @@ def lambda_handler(event, context):
     print(event)
     return {
         'statusCode': 200,
-        'body': json.dumps(body["key1"])
+        'body': json.dumps(body["key2"])
     }
