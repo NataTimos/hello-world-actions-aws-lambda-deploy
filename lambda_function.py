2818a969dff015b8c6eba9c57b0238d88251918a
Adam Grbac <adam.grbac@gmail.com>
2020-09-21 17:54:25 +0100

    Initial Commit

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    body = json.loads(event["body"])
+    print(event)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(body["key1"])
+    }
