76b10c2eb623b4934ac3037e68624a3ca5c72c5b
Nataliia Timoshyna <nezhinskaya83@gmail.com>
2021-08-11 18:26:14 +0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -7,5 +7,5 @@ def lambda_handler(event, context):
 #     return {
 #         'statusCode': 200,
 #         'body': json.dumps(body["key2"])
-    return "hello, world!"
+    return "hello, world-world!"
     }
