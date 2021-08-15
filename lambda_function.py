8152601759ab3158c260bdbb565e9cbe3d855c1d
Nataliia Timoshyna <nezhinskaya83@gmail.com>
2021-08-15 17:29:56 +0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -10,6 +10,7 @@ import json
 import time
 import requests
 import urllib3
+import codecs
 
 # used my api-key
 url ="https://api.airtable.com/v0/appjzQ0kWbadhXzq4/MainTable?fields%5B%5D=ID&fields%5B%5D=title&sort%5B0%5D%5Bfield%5D=ID&api_key=keyL2Fz58E72N4JEa"
@@ -17,23 +18,25 @@ http = urllib3.PoolManager()
 r = http.request('GET', url)
 request_response = json.loads(r.data)
 table_list=[]
+# table_list_length = len(table_list)
 for i in request_response["records"]:
+    # (i["fields"]["title"]).decode('utf-8')
     table_list.append(i["fields"]["title"])
-# print(table_list)
-
+table_list_length = len(table_list)
 temp=[]
 
 def lambda_handler(event, context):
     global temp
     while True:
-        for i in range(len(table_list)):
-            # udaliaem 1 elem v kontse
-            temp.append(table_list.pop(-1))
-            # vstavliaem ego v nachalo
-            table_list.insert(0, temp[0])
+        for i in range(table_list_length):
+            # udaliaem 1 elem v nachale
+            temp.append(table_list.pop(0))
+            # vstavliaem ego v konets
+            table_list.append(temp[0])
             temp=[]
             time.sleep(1)
             # print(table_list[0:3])
  
             return table_list[0:3]
+ 
  
